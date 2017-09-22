cc3df69854d869e752f96ef56b92ba3287d07278
Terrence Wong <wongterrencew@gmail.com>
2017-09-22 12:57:38 +0200

    step 1: markup only

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,28 @@
 import React from 'react'
 
-export default () => <h1>Hello GOV.UK</h1>
+export default () => (
+  <div>
+    <h1>Welcome to GOV.UK</h1>
+    <p>The best place to find government services and information</p>
+    <p>Simpler, clearer, faster</p>
+    <input type='text' placeholder='Search GOV.UK' />
+    <p>Popular on GOV.UK</p>
+    <ul>
+      <li>
+        <a href='#'>Universal Jobmatch job search</a>
+      </li>
+      <li>
+        <a href='#'>Renew vehicle tax</a>
+      </li>
+      <li>
+        <a href='#'>Log in to student finance</a>
+      </li>
+      <li>
+        <a href='#'>Book your theory test</a>
+      </li>
+      <li>
+        <a href='#'>Personal tax account</a>
+      </li>
+    </ul>
+  </div>
+)
